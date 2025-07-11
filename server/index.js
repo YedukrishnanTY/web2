08f0bdc5dc1f0d6b745c49e8c615f89333e2f4fd
yedu.k <yedu345@gmail.com>
2025-07-12 01:03:48 +0530

    client url

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const cors= require("cors");
-const helmet= require("helmet");
-const morgan= require("morgan");
-const express= require("express");
-const details= require("./routes/details.js");
-const coffee= require("./routes/coffee.js");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const express = require("express");
+const details = require("./routes/details.js");
+const coffee = require("./routes/coffee.js");
 require('dotenv').config();
 
 const app = express();
@@ -14,7 +14,13 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(cors());
+const corsOptions = {
+    origin: process.env.CLIENT_URL,  // <-- use .env
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+app.use(cors(corsOptions));
 
 
 app.use("/details", details);
